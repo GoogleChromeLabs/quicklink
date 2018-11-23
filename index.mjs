ce0aa406fcfb1d17db01c9b31e86a83ba88ed3b9
addyosmani <addyosmani@gmail.com>
2018-11-23 12:14:42 -0800

    Adds index: initial implementation

---
+++ index.mjs
@@ -0,0 +1,56 @@
+/**
+ * 
+ * @param urls Array of URLs to prefetch
+ */
+function fetchLinks(urls) {
+    urls.forEach((url) => {
+        prefetch(url);
+    });
+}
+
+/**
+ * Extract only links that are in the visible viewport
+ * @param el DOM element to check for in-viewport links
+ */
+function extractInViewportLinks(el) {
+    return new Promise((resolve, reject) => {
+        const urls = [];
+        const links = el.querySelectorAll('a');
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                // Link is in the view
+                if (entry.intersectionRatio > 0) {
+                    urls.push(entry.target.href);
+                } else {
+                    // Link is out of the view
+                }
+            });
+            resolve(urls);
+        });
+        links.forEach(link => {
+            observer.observe(link);
+        });
+    });
+}
+
+/**
+ * 
+ * @param options 
+ * options.urls: Array of URLs to prefetch (override)
+ * options.el: DOM element to prefetch in-viewport links of
+ */
+export default function quicklink(options) {
+    options = options || {};
+    requestIdleCallback(() => {
+        // Prefetch an array of URLs if supplied (as an override)
+        if (options.urls !== undefined && options.urls.length > 0) {
+            fetchLinks(options.urls);
+        } else {
+            // Element to extract in-viewport links for
+            let el = options.el || document;
+            extractInViewportLinks(el).then((urls) => {
+                fetchLinks(urls);
+            }); 
+        }       
+    });
+}
