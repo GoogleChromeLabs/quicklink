34fa5815ee7b481b0db15e64a8993ebd180c9856
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-06-26 01:08:09 +0300

    upload news with quicklink and news with workbox

---
+++ demos/news-workbox/README.md
@@ -0,0 +1,15 @@
+## Lab: Fetch
+
+In this lab you learn to create and use Fetch
+
+## Getting started
+
+To get started, check out the instructions in the [workshop](http://chrome-enterprise-workshops.firebaseapp.com/labs/workbox).
+
+Original [sourcecode](https://github.com/google-developer-training/pwa-training-labs) and [instructions](https://developers.google.com/web/ilt/pwa/lab-workbox).
+
+## Note
+
+This is not an official Google product.
+
+All images are are licensed CC0 - no attribution required.
---
+++ demos/news-workbox/gulpfile.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2018 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const del = require('del');
+const nunjucks = require('gulp-nunjucks-render');
+const workbox = require('workbox-build');
+const src = `${__dirname}/src`;
+const dist = `${__dirname}/build`;
+
+// Clean "build" directory
+const clean = () => {
+  return del(['build/*'], {dot: true});
+};
+gulp.task('clean', clean);
+
+// Copy "src" directory to "build" directory
+const copy = () => {
+ return gulp.src(['src/**/*', '!src/**/*.html']).pipe(gulp.dest('build'));
+};
+gulp.task('copy', copy);
+
+// Compile HTML
+const html = () => {
+  return gulp.src('src/**/*.html')
+    .pipe(nunjucks({path: 'src'}))
+    .pipe(gulp.dest('build'));
+}
+gulp.task('html', html)
+
+// This is the app's build process
+const build = gulp.series('clean', 'copy', 'html');
+gulp.task('build', build);
+
+// Build the app, run a local dev server, and rebuild on "src" file changes
+const browserSyncOptions = {
+  server: 'build',
+  port: 8081,
+  open: false
+};
+const serve = gulp.series(build, () => {
+  browserSync.init(browserSyncOptions);
+  return gulp.watch('src/**/*', build).on('change', browserSync.reload);
+});
+gulp.task('serve', serve);
+
+// Set the default task to "build"
+gulp.task('default', build);
