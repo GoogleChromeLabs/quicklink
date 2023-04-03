20e11e954b17bf914812493427bd9716ce80e95b
XhmikosR <xhmikosr@gmail.com>
2023-04-03 22:35:02 +0300

    Update ESLint to v8 and add eslint-plugin-react (#321)
    * Update ESLint to v8
    
    * Add eslint-plugin-react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,45 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true
-    },
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 9
-    },
-    extends: 'google',
-    rules: {
-        'max-len': ['warn',
-            {
-                code: 130
-            } // 130 on GitHub, 80 on npmjs.org for README.md code blocks
-        ],
-        'arrow-parens': ['error', 'as-needed'],
-        'space-before-function-paren': [
-            'error',
-            {
-                anonymous: 'always',
-                named: 'never'
-            }
-        ],
-        'no-negated-condition': 'warn',
-        'spaced-comment': ['error', 'always',
-            {
-                exceptions: ['/']
-            }
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 9
+  },
+  extends: [
+    'google',
+    'plugin:react/recommended'
+  ],
+  rules: {
+    'max-len': [
+      'warn',
+      {
+        // 130 on GitHub, 80 on npmjs.org for README.md code blocks
+        code: 130
+      }
+    ],
+    'arrow-parens': [
+      'error',
+      'as-needed'
+    ],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never'
+      }
+    ],
+    'no-negated-condition': 'warn',
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        exceptions: [
+          '/'
         ]
-    },
-    globals: {
-        DOMException: false,
-        URL: false,
-    }
+      }
+    ]
+  }
 };
