c796d3ab2db807af56d6b30fceda8468f16d78e2
XhmikosR <xhmikosr@gmail.com>
2023-04-19 00:18:37 +0300

    Update demos

--- demos/sw.js
+++ demos/sw.js
@@ -1,27 +1,27 @@
 importScripts('https://unpkg.com/network-idle-callback@1.0.1/lib/request-monitor.js');
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', event => {
   console.log('[ServiceWorker] Installed');
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', event => {
   console.log('[ServiceWorker] Activated');
 });
 
-self.addEventListener('fetch', function (e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  self.requestMonitor.listen(e);
+self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] Fetch', event.request.url);
+  self.requestMonitor.listen(event);
 
-  const promise = fetch(e.request)
+  const promise = fetch(event.request)
       .then(response => {
-        console.log('done', e.clientId);
-        self.requestMonitor.unlisten(e);
+        console.log('done', event.clientId);
+        self.requestMonitor.unlisten(event);
         return response;
       })
-      .catch(e => {
+      .catch(error => {
         console.log('error');
-        self.requestMonitor.unlisten(e);
+        self.requestMonitor.unlisten(error);
       });
 
-  e.respondWith(promise);
+  event.respondWith(promise);
 });
