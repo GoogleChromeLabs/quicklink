eedebff5f42649090c6e9752c8a63e0af325a006
XhmikosR <xhmikosr@gmail.com>
2025-12-06 18:51:45 +0200

    Update ESLint to v9

--- site/src/assets/js/script.js
+++ site/src/assets/js/script.js
@@ -1,54 +1,56 @@
-window.addEventListener('load', () => {
+(function () {
   'use strict';
 
-  function initGoToTopBtn() {
-    const goTopBtn = document.querySelector('.back-to-top');
+  window.addEventListener('load', () => {
+    function initGoToTopBtn() {
+      const goTopBtn = document.querySelector('.back-to-top');
 
-    function trackScroll() {
-      const scrolled = window.pageYOffset;
-      const threshold = 400;
+      function trackScroll() {
+        const scrolled = window.pageYOffset;
+        const threshold = 400;
 
-      if (scrolled > threshold) {
-        goTopBtn.classList.remove('hidden');
+        if (scrolled > threshold) {
+          goTopBtn.classList.remove('hidden');
+        }
+        if (scrolled < threshold) {
+          goTopBtn.classList.add('hidden');
+        }
       }
-      if (scrolled < threshold) {
-        goTopBtn.classList.add('hidden');
+
+      function scrollToTop() {
+        const c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (c > 0) {
+          window.requestAnimationFrame(scrollToTop);
+          window.scrollTo(0, c - c / 8);
+        }
       }
+
+      function backToTop() {
+        if (window.pageYOffset > 0) {
+          scrollToTop();
+        }
+      }
+
+      window.addEventListener('scroll', trackScroll, {passive: true});
+      goTopBtn.addEventListener('click', backToTop);
     }
 
-    function scrollToTop() {
-      const c = document.documentElement.scrollTop || document.body.scrollTop;
-      if (c > 0) {
-        window.requestAnimationFrame(scrollToTop);
-        window.scrollTo(0, c - c / 8);
-      }
-    }
+    initGoToTopBtn();
 
-    function backToTop() {
-      if (window.pageYOffset > 0) {
-        scrollToTop();
-      }
-    }
+    const clipboard = new ClipboardJS('#copy-snippet-button', {
+      text: trigger => trigger.parentNode.previousElementSibling.textContent.trim(),
+    });
 
-    window.addEventListener('scroll', trackScroll, {passive: true});
-    goTopBtn.addEventListener('click', backToTop);
-  }
+    clipboard.on('success', event => {
+      event.clearSelection();
+      event.trigger.blur();
+      const notifyCopiedSnippet = document.querySelector('.notify-copied-snippet');
+      notifyCopiedSnippet.classList.add('notify-copied-snippet--displayed');
+    });
 
-  initGoToTopBtn();
-
-  const clipboard = new ClipboardJS('#copy-snippet-button', {
-    text: trigger => trigger.parentNode.previousElementSibling.textContent.trim(),
+    clipboard.on('error', event => {
+      console.error('[clipboard error] Action:', event.action);
+      console.error('[clipboard error] Trigger:', event.trigger);
+    });
   });
-
-  clipboard.on('success', event => {
-    event.clearSelection();
-    event.trigger.blur();
-    const notifyCopiedSnippet = document.querySelector('.notify-copied-snippet');
-    notifyCopiedSnippet.classList.add('notify-copied-snippet--displayed');
-  });
-
-  clipboard.on('error', event => {
-    console.error('[clipboard error] Action:', event.action);
-    console.error('[clipboard error] Trigger:', event.trigger);
-  });
-});
+})();
